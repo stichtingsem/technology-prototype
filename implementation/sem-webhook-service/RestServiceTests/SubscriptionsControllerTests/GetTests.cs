65c64b98bcb7f8a932d2e279479000213bd6667b
René Wiersma <rene.wiersma@noordhoff.nl>
2021-02-12 12:08:11 +0100

    Add default test for subscriptionscontroller

---
+++ implementation/sem-webhook-service/RestServiceTests/SubscriptionsControllerTests/GetTests.cs
@@ -0,0 +1,18 @@
+﻿using NUnit.Framework;
+using RestService.Subscriptions;
+
+namespace RestServiceTests.SubscriptionsControllerTests
+{
+    public class GetTests
+    {
+        [Test]
+        public void Default()
+        {
+            var sut = new SubscriptionsController();
+
+            var result = sut.Get();
+
+            Assert.IsNotNull(result);
+        }
+    }
+}
