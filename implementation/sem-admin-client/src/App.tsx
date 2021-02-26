0e5b9a69dd4d07e624a101b43c49e03f42322b35
Henk Haarsma <Henk.Haarsma@noordhoff.nl>
2021-02-26 11:59:04 +0100

    Get config from config file

--- implementation/sem-admin-client/src/App.tsx
+++ implementation/sem-admin-client/src/App.tsx
@@ -9,35 +9,46 @@ import {
 } from './components'
 import './App.css';
 
+function getClientConfig() {
+  return fetch('/client_config.json')
+    .then((response) => { return response.json() })
+    .catch((error) => {
+      console.error(error);
+    });
+}
+
 function App() {
-  const idpUrl = process.env.REACT_APP_IDP_URL || '';
-  const avatarConfig: IClientConfig = {
-    clientId: 'IDP.Playground',
-    scope: 'openid fullname profile email role offline_access',
-    idpUrl: idpUrl
-  }
+  const clientConfig = getClientConfig();
+  clientConfig.then(config => {
+    const idpUrl = config.IdpUrl;
+    const avatarConfig: IClientConfig = {
+      clientId: 'IDP.Playground',
+      scope: 'openid fullname profile email role offline_access',
+      idpUrl: idpUrl
+    }
 
-  const [visibleSection, setVisibleSection] = useState<string>('view')
+    const [visibleSection, setVisibleSection] = useState<string>('view')
 
-  return (
-    <div>
-      <header className="header">
-        <nav>
-          <span className="navbar_link" onClick={() => setVisibleSection('view')}>View</span>
-          <span className="navbar_link" onClick={() => setVisibleSection('create')}>Create</span>
-          <span className="navbar_link" onClick={() => setVisibleSection('update')}>Update</span>
-          <span className="navbar_link" onClick={() => setVisibleSection('delete')}>Delete</span>
-        </nav>
-        <IDP {...avatarConfig} />
-      </header>
-      <div className="content">
-        {visibleSection === 'view' && <SubscriptionsList />}
-        {visibleSection === 'create' && <CreateSubscriptionForm />}
-        {visibleSection === 'update' && <UpdateSubscriptionForm />}
-        {visibleSection === 'delete' && <DeleteSubscriptionForm />}
+    return (
+      <div>
+        <header className="header">
+          <nav>
+            <span className="navbar_link" onClick={() => setVisibleSection('view')}>View</span>
+            <span className="navbar_link" onClick={() => setVisibleSection('create')}>Create</span>
+            <span className="navbar_link" onClick={() => setVisibleSection('update')}>Update</span>
+            <span className="navbar_link" onClick={() => setVisibleSection('delete')}>Delete</span>
+          </nav>
+          <IDP {...avatarConfig} />
+        </header>
+        <div className="content">
+          {visibleSection === 'view' && <SubscriptionsList />}
+          {visibleSection === 'create' && <CreateSubscriptionForm />}
+          {visibleSection === 'update' && <UpdateSubscriptionForm />}
+          {visibleSection === 'delete' && <DeleteSubscriptionForm />}
+        </div>
       </div>
-    </div>
-  );
+    );
+  })
 }
 
 export default App;
